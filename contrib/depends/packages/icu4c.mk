2473dfc9f0660cddaec474a4bdd7279c62596107
tobtoht <tob@featherwallet.org>
2023-05-23 16:35:15 +0200

    depends: update icu4c to 73.1

--- contrib/depends/packages/icu4c.mk
+++ contrib/depends/packages/icu4c.mk
@@ -1,12 +1,13 @@
 package=icu4c
-$(package)_version=55.2
-$(package)_download_path=https://github.com/unicode-org/icu/releases/download/release-55-2/
-$(package)_file_name=$(package)-55_2-src.tgz
-$(package)_sha256_hash=eda2aa9f9c787748a2e2d310590720ca8bcc6252adf6b4cfb03b65bef9d66759
+$(package)_version=73.1
+$(package)_download_path=https://github.com/unicode-org/icu/releases/download/release-73-1/
+$(package)_file_name=$(package)-73_1-src.tgz
+$(package)_sha256_hash=a457431de164b4aa7eca00ed134d00dfbf88a77c6986a10ae7774fc076bb8c45
 $(package)_patches=icu-001-dont-build-static-dynamic-twice.patch
 
 define $(package)_set_vars
-  $(package)_build_opts=CFLAGS="$($(package)_cflags) $($(package)_cppflags) -DU_USING_ICU_NAMESPACE=0 -DU_STATIC_IMPLEMENTATION -DU_COMBINED_IMPLEMENTATION -fPIC -DENABLE_STATIC=YES -DPGKDATA_MODE=static"
+  $(package)_build_opts=CFLAGS="$($(package)_cflags) $($(package)_cppflags) -std=c++11 -DU_USING_ICU_NAMESPACE=0 -DU_STATIC_IMPLEMENTATION -DU_COMBINED_IMPLEMENTATION -fPIC -DENABLE_STATIC=YES -DPGKDATA_MODE=static"
+  $(package)_cxxflags=-std=c++11
 endef
 
 define $(package)_config_cmds
@@ -17,8 +18,8 @@ define $(package)_config_cmds
   sh ../source/runConfigureICU Linux &&\
   make &&\
   cd ../buildb &&\
-  sh ../source/runConfigureICU MinGW --enable-static=yes --disable-shared --disable-layout --disable-layoutex --disable-tests --disable-samples --prefix=$(host_prefix) --with-cross-build=`pwd`/../builda &&\
-  $(MAKE) $($(package)_build_opts)
+  C_STANDARD="c11" CXX_STANDARD="c++11" sh ../source/runConfigureICU MinGW --enable-static=yes --disable-shared --disable-layout --disable-layoutex --disable-tests --disable-samples --prefix=$(host_prefix) --with-cross-build=`pwd`/../builda &&\
+  C_STANDARD="c11" CXX_STANDARD="c++11" $(MAKE) C_STANDARD="c11" CXX_STANDARD="c++11" $($(package)_build_opts)
 endef
 
 define $(package)_stage_cmds
