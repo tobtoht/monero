6216921b370cafca1d5470a8b3f16511f08071cd
tobtoht <tob@featherwallet.org>
2023-07-02 21:00:24 +0200

    Bootstrappable Builds

---
+++ contrib/guix/libexec/check.bash
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+export LC_ALL=C
+set -e -o pipefail
+
+check_string() {
+    local binary="$1"
+    local string="$2"
+
+    if strings "$binary" | grep -q "${string}"; then
+        echo "Error: ${binary} contains unexpected string: \"${string}\""
+        exit 1
+    fi
+}
+
+check_rpath() {
+    local binary="$1"
+
+    rpath=$(patchelf --print-rpath "$binary")
+    if [ ! -z "$rpath" ]; then
+        echo "Error: ${binary} contains unexpected rpath: ${rpath}"
+        exit 1
+    fi
+}
+
+check_interpreter() {
+    local binary="$1"
+    local allowed_interpreter="$2"
+
+    interpreter=$(patchelf --print-interpreter "$binary")
+    if [ "$interpreter" != "$allowed_interpreter" ]; then
+        echo "Error: ${binary} contains unexpected interpreter: ${interpreter}, should be: ${allowed_interpreter}"
+        exit 1
+    fi
+}
+
+check_libraries() {
+    local binary="$1"
+    shift
+    allowed_dynamic_libraries=("$@")
+
+    dynamic_libraries=($(objdump -p "$binary" | grep "NEEDED" | awk '{print $2}'))
+
+    for library in "${dynamic_libraries[@]}"; do
+        found=0
+        for allowed_library in "${allowed_dynamic_libraries[@]}"; do
+            if [[ "$library" == "$allowed_library" ]]; then
+                found=1
+                break
+            fi
+        done
+        if [ "$found" -eq 0 ]; then
+            echo "Error: ${binary} links against unexpected library: ${library}"
+            exit 1
+        fi
+    done
+}
