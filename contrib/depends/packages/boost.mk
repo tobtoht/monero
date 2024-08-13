ac0a655547a6d12267fa22df551ffa23096f8158
tobtoht <tob@featherwallet.org>
2024-08-13 19:10:17 +0200

    test

--- contrib/depends/packages/boost.mk
+++ contrib/depends/packages/boost.mk
@@ -24,8 +24,9 @@ $(package)_archiver_darwin=$($(package)_libtool)
 $(package)_config_libraries_$(host_os)="chrono,filesystem,program_options,system,thread,test,date_time,regex,serialization"
 $(package)_config_libraries_mingw32="chrono,filesystem,program_options,system,thread,test,date_time,regex,serialization,locale"
 $(package)_cxxflags+=-std=c++11
-$(package)_cxxflags_linux+=-fPIC -ffile-prefix-map=$($(package)_extract_dir)=/usr
+$(package)_cxxflags_linux+=-fPIC
 $(package)_cxxflags_freebsd+=-fPIC
+$(package)_cxxflags_darwin+=-ffile-prefix-map=$($(package)_extract_dir)=/usr
 endef
 
 define $(package)_preprocess_cmds
