02aff2e5ebc7ecb205eacadd2ebc84d5a0f3d3cb
tobtoht <tob@featherwallet.org>
2024-08-25 12:32:48 +0200

    depends: android_ndk: bump to 27 (latest LTS)

--- contrib/depends/packages/android_ndk.mk
+++ contrib/depends/packages/android_ndk.mk
@@ -1,8 +1,8 @@
 package=android_ndk
-$(package)_version=26d
+$(package)_version=27
 $(package)_download_path=https://dl.google.com/android/repository/
 $(package)_file_name=android-ndk-r$($(package)_version)-linux.zip
-$(package)_sha256_hash=eefeafe7ccf177de7cc57158da585e7af119bb7504a63604ad719e4b2a328b54
+$(package)_sha256_hash=2f17eb8bcbfdc40201c0b36e9a70826fcd2524ab7a2a235e2c71186c302da1dc
 
 define $(package)_extract_cmds
   echo $($(package)_sha256_hash) $($(1)_source_dir)/$($(package)_file_name) | sha256sum -c &&\
