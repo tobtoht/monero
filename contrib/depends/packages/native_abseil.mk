f94f96a3aa589acba3c08ada279dec43a81a6f44
tobtoht <tob@featherwallet.org>
2025-01-03 12:06:55 +0100

    test

--- contrib/depends/packages/native_abseil.mk
+++ contrib/depends/packages/native_abseil.mk
@@ -5,6 +5,10 @@ $(package)_download_file=$($(package)_version).tar.gz
 $(package)_file_name=abseil-$($(package)_version).tar.gz
 $(package)_sha256_hash=f50e5ac311a81382da7fa75b97310e4b9006474f9560ac46f54a9967f07d4ae3
 
+define $(package)_set_vars
+  $(package)_cxxflags+=-std=c++17
+endef
+
 define $(package)_config_cmds
   $($(package)_cmake)
 endef
