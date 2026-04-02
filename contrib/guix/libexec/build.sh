f6bd9c7c6552ffcb1e6dde132e3ef302236b6f17
tobtoht <tob@featherwallet.org>
2026-04-02 18:46:26 +0000

    depends: extract cached package as self

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -208,7 +208,7 @@ esac
 export GLIBC_DYNAMIC_LINKER=${glibc_dynamic_linker}
 
 # Environment variables for determinism
-export TAR_OPTIONS="--owner=0 --group=0 --numeric-owner --mtime='@${SOURCE_DATE_EPOCH}' --sort=name"
+export TAR_OPTIONS="--no-same-owner --owner=0 --group=0 --numeric-owner --mtime='@${SOURCE_DATE_EPOCH}' --sort=name"
 export TZ="UTC"
 
 ####################
