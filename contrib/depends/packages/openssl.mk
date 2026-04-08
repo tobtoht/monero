6dad5ab5f6f3dd77859eaa866de0363ff908ebaf
tobtoht <tob@featherwallet.org>
2026-04-08 10:53:33 +0200

    depends: openssl: update to 3.5.6

--- contrib/depends/packages/openssl.mk
+++ contrib/depends/packages/openssl.mk
@@ -1,8 +1,8 @@
 package=openssl
-$(package)_version=3.5.4
+$(package)_version=3.5.6
 $(package)_download_path=https://github.com/openssl/openssl/releases/download/openssl-$($(package)_version)
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=967311f84955316969bdb1d8d4b983718ef42338639c621ec4c34fddef355e99
+$(package)_sha256_hash=deae7c80cba99c4b4f940ecadb3c3338b13cb77418409238e57d7f31f2a3b736
 $(package)_patches=fix-android.patch
 
 define $(package)_set_vars
